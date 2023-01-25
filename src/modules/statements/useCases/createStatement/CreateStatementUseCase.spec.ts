086e5d393488d892c6af40c02c338c3aad3d935b
Daniel Moura <daniel-moura@outlook.com>
2023-01-24 21:41:47 -0300

    test: init create statement

---
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
+
+let inMemoryUsersRepository : InMemoryUsersRepository;
+let inMemoryStatementsRepository : InMemoryStatementsRepository;
+let createStatementUseCase: CreateStatementUseCase;
+let createUserUseCase : CreateUserUseCase;
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
+
+describe('Create Statement', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    inMemoryStatementsRepository = new InMemoryStatementsRepository();
+    createStatementUseCase = new CreateStatementUseCase(inMemoryUsersRepository, inMemoryStatementsRepository);
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  });
+
+});
