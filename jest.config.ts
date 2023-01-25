b793be592adacc40029c4e1838a7e8cfa8b6cd1e
Daniel Moura <daniel-moura@outlook.com>
2023-01-24 21:38:57 -0300

    feat: config coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 export default {
   bail: true,
   clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
   preset: 'ts-jest',
   testMatch: ['**/*.spec.ts'],
 };
